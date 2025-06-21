2835ae84b883fa8a8e037107cd4de85f958eee85
krisha0806 <krishnansudhiksha@gmail.com>
2025-06-22 01:45:18 +0530

    Week 1 hands-on code and output

---
+++ Week 1/02_Algorithms_DataStructures/01_EcommercePlatformSearchFunction/Code/LinearSearch.java
@@ -0,0 +1,10 @@
+public class LinearSearch {
+    public static Product search(Product[] products, String targetName) {
+        for (Product p : products) {
+            if (p.productName.equalsIgnoreCase(targetName)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
