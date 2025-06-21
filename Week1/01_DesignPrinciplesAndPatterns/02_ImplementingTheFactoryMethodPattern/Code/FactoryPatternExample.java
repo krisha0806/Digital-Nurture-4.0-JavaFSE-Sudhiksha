7385989a757e241d5222c145c38d46a42d8f471a
krisha0806 <krishnansudhiksha@gmail.com>
2025-06-21 22:22:28 +0530

    Hands-on under 'Week 1'

---
+++ Week1/01_DesignPrinciplesAndPatterns/02_ImplementingTheFactoryMethodPattern/Code/FactoryPatternExample.java
@@ -0,0 +1,39 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Square implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Square");
+    }
+}
+
+class ShapeFactory {
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) return null;
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
+
+public class FactoryPatternExample {
+    public static void main(String[] args) {
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape shape1 = factory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = factory.getShape("SQUARE");
+        shape2.draw();
+    }
+}
